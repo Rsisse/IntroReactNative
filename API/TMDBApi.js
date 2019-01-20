9c47ccd79ca850ee7825da64c83c7e64bc0d10b6
Enzo <Constant Enzo>
2019-01-20 10:45:19 +0100

    first commit

---
+++ API/TMDBApi.js
@@ -0,0 +1,13 @@
+
+const API_TOKEN="8b89dfd1989f7434ec772c00253495dd";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+  }
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
